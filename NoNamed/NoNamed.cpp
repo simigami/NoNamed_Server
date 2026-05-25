6eeedb58b66638567ce451306921e43c1eba841b
simigami <46891822+simigami@users.noreply.github.com>
2026-05-25 23:31:50 +0900

    feat: add thread tutorial

--- NoNamed/NoNamed.cpp
+++ NoNamed/NoNamed.cpp
@@ -5,7 +5,40 @@
 #include "NoNamed.h"
 #include <CorePch.cpp>
 
+#include <thread> // Cpp11 Universal Thread Creation
+
+void HelloThread()
+{
+	cout << "Hello Thread" << endl;
+}
+
+void HelloThread2(int num)
+{
+	cout << "Hello Thread 2 " << num << endl;
+}
+
 int main()
 {
-	HelloWorld();
+	vector<std::thread> v;
+
+	std::thread t;
+	int32 count = t.hardware_concurrency(); // Number of threads ~= Number of CPU Cores
+	cout << "core count is : " << count << endl; // id is 0 when theres no job
+
+	for(int32 i = 0; i< 10; i++)
+	{
+		v.push_back(std::thread(HelloThread2, i));  // give thread t a job
+
+		std::thread::id id = v[i].get_id(); // id is sth when theres some job working on
+		cout << "Id of v" << i << " is : " << id << endl;	}
+
+	for(int32 i = 0; i< 10; i++)
+	{
+		if(v[i].joinable()) // Check if thread is able to join, true if joinable
+		{
+			v[i].join();
+		}
+	}
+
+	//t.detach(); // Detach Thread from its parent, becomes an independant thread (aka LINUX Daemon)
 }
