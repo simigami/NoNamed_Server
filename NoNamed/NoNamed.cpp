c8f5b76aecdbaedf1f1c3ac0ca02146b785f6234
simigami <46891822+simigami@users.noreply.github.com>
2026-05-26 16:13:31 +0900

    feat: add atomic tutorial

--- NoNamed/NoNamed.cpp
+++ NoNamed/NoNamed.cpp
@@ -5,7 +5,9 @@
 #include "NoNamed.h"
 #include <CorePch.cpp>
 
+#include <chrono> // Time Comparison
 #include <thread> // Cpp11 Universal Thread Creation
+#include <atomic> // Cpp 11 Universal Atomic Transaction
 
 void HelloThread()
 {
@@ -17,28 +19,55 @@ void HelloThread2(int num)
 	cout << "Hello Thread 2 " << num << endl;
 }
 
+atomic<int32> sum = 0;
+
+void add()
+{
+	for(int32 i =0; i < 100'0000; i++)
+	{
+		//sum++; // this is 3 instruction in assembly
+		sum.fetch_add(1); // you can also arithmetic value with this method
+
+		// move eax, dword ptr
+		// inc eax (+= 1)
+		// move dword ptr, eax
+	}
+}
+
+void sub()
+{
+	for(int32 i =0; i < 100'0000; i++)
+	{
+		sum--;
+	}
+}
+
 int main()
 {
 	vector<std::thread> v;
 
-	std::thread t;
-	int32 count = t.hardware_concurrency(); // Number of threads ~= Number of CPU Cores
-	cout << "core count is : " << count << endl; // id is 0 when theres no job
+	auto start = chrono::high_resolution_clock::now();
 
-	for(int32 i = 0; i< 10; i++)
-	{
-		v.push_back(std::thread(HelloThread2, i));  // give thread t a job
+	add();
+	sub();
 
-		std::thread::id id = v[i].get_id(); // id is sth when theres some job working on
-		cout << "Id of v" << i << " is : " << id << endl;	}
+	auto end = chrono::high_resolution_clock::now();
+	chrono::duration<double, milli> ms_double = end - start;
 
-	for(int32 i = 0; i< 10; i++)
-	{
-		if(v[i].joinable()) // Check if thread is able to join, true if joinable
-		{
-			v[i].join();
-		}
-	}
+	cout << "sum is : " << sum << endl; // sum is absolutely 0
+	cout << "time is : " << ms_double.count()  << " ms..." << endl;
 
-	//t.detach(); // Detach Thread from its parent, becomes an independant thread (aka LINUX Daemon)
+	start = chrono::high_resolution_clock::now();
+
+	std::thread t1(add);
+	std::thread t2(sub);
+
+	t1.join();
+	t2.join();
+
+	end = chrono::high_resolution_clock::now();
+	ms_double = end - start;
+
+	cout << "sum is : " << sum << endl; // sum is usually not 0 (?)
+	cout << "time is : " << ms_double.count()  << " ms..." << endl;
 }
